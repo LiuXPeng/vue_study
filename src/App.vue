5891ae4dd433ec553a8296f4ff02294b1664de11
LiuXPeng <703762445@qq.com>
2025-10-09 23:42:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <!-- <h1>谁最美丽可爱</h1> -->
+    <Person />
+
+</template>
+
+<script lang="ts">
+import Person from './components/Person.vue'
+export default {
+  // name: 'App',
+  components: { Person }
+}
+</script>
+
+<style>
+.app {
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button{
+  margin: 0 5px;
+}
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age  }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Person',
+    setup(){
+        let name = 'zhangsan'
+        let age = 18
+        let tel = 12232312
+
+        function changeName(){
+            name = 'lisi'
+        }
+
+        function changeAge(){
+            age+=1
+        }
+
+        function showTel(){
+            alert(tel)
+        }
+
+        return {name, age, changeName, changeAge, showTel}
+
+        // return function(){
+        //     return "haha"
+        // }
+    }
+}
+</script>
+
+<style scoped></style>
